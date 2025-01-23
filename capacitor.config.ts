8def808e0b7ee854c872785395a0b0656ec061ee
Queen Lisa <122045306+QueenLisaOmondi@users.noreply.github.com>
2025-01-23 11:51:38 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Kitchen_Ninja',
+  webDir: 'www'
+};
+
+export default config;
